b17265ead52191fb1a3bf8f15c0bf23d88120174
Anton Rumiantsev <anton.rumiantsev@macys.com>
2020-06-10 17:34:45 +0300

    Fix styles

--- app/src/main/java/com/griddynamics/connectedapps/ui/history/DataHistoryFragment.kt
+++ app/src/main/java/com/griddynamics/connectedapps/ui/history/DataHistoryFragment.kt
@@ -16,6 +16,8 @@ import com.griddynamics.connectedapps.viewmodels.ViewModelFactory
 import dagger.android.support.AndroidSupportInjection
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_data_history.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import kotlinx.android.synthetic.main.header_layout.view.*
 import javax.inject.Inject
 
 private const val TAG: String = "DataHistoryFragment"
@@ -23,6 +25,8 @@ private const val TAG: String = "DataHistoryFragment"
 class DataHistoryFragment : DaggerFragment() {
     private lateinit var viewModel: HistoryFragmentViewModel
     private lateinit var adapter: TabAdapter
+    private var address: String? = null
+    private var deviceId: String? = null
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -41,6 +45,14 @@ class DataHistoryFragment : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            deviceId = DataHistoryFragmentArgs.fromBundle(it).device
+            address = DataHistoryFragmentArgs.fromBundle(it).address
+        }
+        history_header.ib_header_back_arrow.setOnClickListener { requireActivity().onBackPressed() }
+        history_header.tv_header_title.text = getString(R.string.history)
+        history_header.ll_header_extra_container.visibility = View.VISIBLE
+        history_header.tv_header_address.text = address
         viewModel = ViewModelProvider(this, viewModelFactory)[HistoryFragmentViewModel::class.java]
         adapter = TabAdapter(this)
         adapter.addFragment(HourHistoryFragment(), "Now")

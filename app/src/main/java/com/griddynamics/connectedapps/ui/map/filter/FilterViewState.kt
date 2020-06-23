0e190e22e88b8634a0204f9e46561d39cf992d08
Anton Rumiantsev <anton.rumiantsev@macys.com>
2020-06-23 15:27:19 +0300

    Make filter radio button like

--- app/src/main/java/com/griddynamics/connectedapps/ui/map/filter/FilterViewState.kt
+++ app/src/main/java/com/griddynamics/connectedapps/ui/map/filter/FilterViewState.kt
@@ -1,13 +1,55 @@
 package com.griddynamics.connectedapps.ui.map.filter
 
+import androidx.databinding.Observable
 import androidx.databinding.ObservableBoolean
 
 class FilterViewState {
-    val isAll = ObservableBoolean()
-    val isCo2 = ObservableBoolean()
-    val isTemp = ObservableBoolean()
-    val isHumidity = ObservableBoolean()
-    val isPm25 = ObservableBoolean()
-    val isPm1 = ObservableBoolean()
-    val isPm10 = ObservableBoolean()
+    private val properties = mutableListOf<ObservableBoolean>()
+    private lateinit var propertyChangedListener: Observable.OnPropertyChangedCallback
+
+    init {
+        propertyChangedListener = object : Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable, propertyId: Int) {
+                for (property in properties) {
+                    property.removeOnPropertyChangedCallback(propertyChangedListener)
+                    if (sender == property) {
+                        property.set(true)
+                    } else {
+                        property.set(false)
+                    }
+                    property.addOnPropertyChangedCallback(propertyChangedListener)
+                }
+            }
+
+        }
+    }
+
+    val isAll = ObservableBoolean(true).apply {
+        properties.add(this)
+        this.addOnPropertyChangedCallback(propertyChangedListener)
+    }
+    val isCo2 = ObservableBoolean().apply {
+        properties.add(this)
+        this.addOnPropertyChangedCallback(propertyChangedListener)
+    }
+    val isTemp = ObservableBoolean().apply {
+        properties.add(this)
+        this.addOnPropertyChangedCallback(propertyChangedListener)
+    }
+    val isHumidity = ObservableBoolean().apply {
+        properties.add(this)
+        this.addOnPropertyChangedCallback(propertyChangedListener)
+    }
+    val isPm25 = ObservableBoolean().apply {
+        properties.add(this)
+        this.addOnPropertyChangedCallback(propertyChangedListener)
+    }
+    val isPm1 = ObservableBoolean().apply {
+        properties.add(this)
+        this.addOnPropertyChangedCallback(propertyChangedListener)
+    }
+    val isPm10 = ObservableBoolean().apply {
+        properties.add(this)
+        this.addOnPropertyChangedCallback(propertyChangedListener)
+    }
 }
